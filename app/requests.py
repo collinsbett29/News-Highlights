06fd5bba8c2dccd51fa2d20450418a7806dda9b5
collinsbett29 <collinsbett29@gmail.com>
2019-10-13 22:53:54 +0300

    Define configure_request

--- app/requests.py
+++ app/requests.py
@@ -5,3 +5,8 @@ from .models import News
 api_key = None
 # Getting the news base url
 base_url = None
+
+def configure_request(app):
+    global api_key,base_url
+    api_key = app.config['NEWS_API_KEY']
+    base_url = app.config['MOVIE_API_BASE_URL']
