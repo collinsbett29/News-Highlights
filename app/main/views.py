52392d7e931c7e66521570f2fea15f6878c01c39
collinsbett29 <collinsbett29@gmail.com>
2019-10-14 12:07:34 +0300

    Create a new file

--- app/main/views.py
+++ app/main/views.py
@@ -5,5 +5,19 @@ from .forms import ReviewForm
 from ..models import Review
 
 # views
-@main.route
+@main.route('/')
 def index():
+    '''
+    View rooot page function that returns index page and its data
+    '''
+    # Getting news sources
+    business_news = get_news('business')
+    entertainment_news = get_news('entertainment')
+    health_news = get_news('health')
+    science_news = get_news('science')
+    sports_news = get_news('sports')
+    technology_news = get_news('technology')
+    
+    title = 'Home - Welcome to the Updated News Website'
+    return render_template('index.html',business = business_news,entertainment = entertainment_news,health = health_news,science = science_news,sports = sports_news,technology = technology_news)
+
