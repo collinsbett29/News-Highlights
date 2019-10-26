6918caac64dc62b2f59f337c5de89b650250eba0
collinsbett29 <collinsbett29@gmail.com>
2019-10-26 10:12:20 +0300

    Add the articles

--- app/main/views.py
+++ app/main/views.py
@@ -1,24 +1,30 @@
-from flask import render_template,request,redirect,url_for
+from flask import  render_template,redirect,url_for,request
 from . import main
 from ..models import Sources
 from ..requests import get_sources, get_articles
 
-# views
+
+#Views
 @main.route('/')
 def index():
+    
     '''
+    
     View root page function that returns the index page and  its data
+    
     '''
     sources = get_sources()
     print(sources)
     
-    title = 'Sources - Some of the best news sources'
+    title = 'Home - Some of the best news sources'
     return render_template('index.html', title = title, sources = sources)
 
 @main.route('/news')
 def news():
+    
     '''
-    Views the news page functionality that returns news highlights
+    Views the news page functionality that returns newshighlights details and its data
+    
     '''
     articles = get_articles()
     title = 'Articles - Some of the articles'
