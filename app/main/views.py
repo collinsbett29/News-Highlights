9217035237b2695f9ff953b143e52848f86baf8e
collinsbett29 <collinsbett29@gmail.com>
2019-10-30 21:22:16 +0300

    Initialize the app

--- app/main/views.py
+++ app/main/views.py
@@ -1,31 +1,31 @@
-from flask import  render_template,redirect,url_for,request
+from flask import render_template
 from . import main
-from ..models import Sources
-from ..requests import get_sources, get_articles
+from ..request import get_news,get_details
+#views
 
 
-#Views
 @main.route('/')
 def index():
-    
     '''
-    
-    View root page function that returns the index page and  its data
-    
+    function that returns index page
     '''
-    sources = get_sources()
-    print(sources)
-    
-    title = 'Home - Some of the best news sources'
-    return render_template('index.html', title = title, sources = sources)
 
-@main.route('/news')
-def news():
+    general_list = get_news('us', 'general')
+    business_list = get_news('us', 'business')
+    technology_list = get_news('us', 'technology')
+    sports_list = get_news('us', 'sports')
+    health_list = get_news('us', 'health')
+    science_list = get_news('us', 'science')
+    entertainment_list = get_news('us', 'entertainment')
     
-    '''
-    Views the news page functionality that returns newshighlights details and its data
-    
-    '''
-    articles = get_articles()
-    title = 'Articles - Some of the articles'
-    return render_template('news.html', articles=articles, title = title)
+    return render_template('index.html',general=general_list,business=business_list,technology=technology_list,sports=sports_list,health=health_list,science=science_list,entertainment=entertainment_list)
+   
+@main.route('/news/<id>')
+def news(id):
+    """
+    View articles page that returns the news article from a highlight
+    """
+    news_args = get_details(id)
+    highlight_args = 'Route Working!!'
+    # name = f'{results_list}'
+    return render_template('news.html',highlight_param=highlight_args,news=news_args)
--- app/models.py
+++ app/models.py
@@ -1,28 +1,26 @@
-class Sources:
-    '''
-    Source class to define News objects
-    '''
-
-    def __init__(self,id,name,title,author,url,urlToImage,publishedAt):
-        self.id = id
-        self.name = name
-        self.title = title
-        self.author = author
-        self.url = url
-        self.urlToImage = urlToImage
-        self.publishedAt = publishedAt
-
 class Articles:
     '''
-    Article class to define article object
+    define news objects
     '''
-    def __init__(self,id,name,author,title,content,description,url,urlToImage,publishedAt):
 
+    def __init__(self,d,name,author,title,description,url,urlToImage,publishedAt):
+        self.id = id
+        self.name =name
+        self.author =author
+        self.title =title
+        self.description =description
+        self.url =url
+        self.urlToImage =urlToImage
+        self.publishedAt =publishedAt
+class Source:
+    '''
+    define source
+            '''
+    def __init__(self, id, name, description, url, category, country,title):
         self.id = id
         self.name = name
-        self.title = title
-        self.description = description
+        self.description = description  
         self.url = url
-        self.urlToImage = urlToImage
-        self.publishedAt = publishedAt
-        self.content = content
+        self.category = category
+        self.title =title
+        self.country= country
