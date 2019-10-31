4d3e11ff0b225578edc09b88f5700b61005e26a5
collinsbett29 <collinsbett29@gmail.com>
2019-10-31 12:17:03 +0300

    Deployment to heroku master

--- config.py
+++ config.py
@@ -4,12 +4,12 @@ class Config:
 
     NEWS_API_SOURCES_URL = 'https://newsapi.org/v2/sources?language=&country={}&apiKey={}'
     ARTICLE_API_BASE_URL = 'https://newsapi.org/v2/everything?sources={}&apiKey={}'
-    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
-    # NEWS_API_KEY_URL='https://newsapi.org/v2/sources?apiKey={}'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')'
     
    
 
 class ProdConfig(Config):
+    pass
     
 
 
